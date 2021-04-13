867d539e86273018c011066f29cd6ab2c4b91ef3
Andrey Brekotkin <brekot@ya.ru>
2021-04-13 18:54:14 +0300

    Index page Full

---
+++ README.md
@@ -0,0 +1,24 @@
+<a href="/index.html">Главная страница</a><br>
+
+<hr>
+
+Папка /docs - для разработчика. Содержит готовые файлы верски. Остальные файлы и папки служебные.
+
+<hr>
+
+<b>Подключены в JS:</b><br>
+<ul>
+    <li>jQuery</li>
+    <li>Swiper - для слайдеров (https://swiperjs.com/)</li>
+    <li>formStyler - для стилизации select (http://dimox.name/jquery-form-styler/)</li>
+    <li>FancyBox - для окон (https://fancyapps.com/fancybox/3/)</li>
+</ul>
+
+<hr>
+
+<b>Bootstrap используются только классы:</b><br>
+<ul>
+    <li>.container</li>
+    <li>.d-*-none</li>
+    <li>.d-*-block</li>
+</ul>
--- src/js/index.js
+++ src/js/index.js
@@ -1,8 +1,54 @@
 import $ from "jquery";
-import formstyler from './jquery.formstyler.js';
+import formstyler from './lib/jquery.formstyler.js';
+import Swiper from './lib/swiper.js';
+
+window.jQuery = $;
+window.$ = $;
+
+require('@fancyapps/fancybox');
 
 $(function() {
 
     // Стилизация элементов
     $('.select-style').styler();
+
+    /* - - - Подключение fancybox - - - */
+    $('[data-fancybox]').fancybox({
+        buttons: [
+            "zoom",
+            //"share",
+            "slideShow",
+            "fullScreen",
+            //"download",
+            //"thumbs",
+            "close"
+        ],
+    });
+
+    // Раскрытие | Скрытие меню
+    $('.menu-top-btn').on('click', function(){
+
+        $('.menu-top').addClass('menu-top_active');
+    });
+
+    $('.menu-top__close').on('click', function(){
+
+        $('.menu-top').removeClass('menu-top_active');
+    });
+
+    $(document).on('mouseup', function (e){
+
+		if (!$(".menu-top").is(e.target) && $(".menu-top").has(e.target).length === 0)
+        {
+			$('.menu-top').removeClass('menu-top_active');
+		}
+	});
+
+    new Swiper('.product-slider', {
+        spaceBetween: 30,
+        navigation: {
+            nextEl: '.product-slider__next',
+            disabledClass: 'product-slider__str_disabled'
+        },
+    });
 });
